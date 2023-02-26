821219f32304030d07ed11925344714224d2987e
Onnisan <38378848+Onnisan@users.noreply.github.com>
2023-02-27 00:31:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,18 +21,18 @@ app.listen(PORT, () => {
 
 
 // For outside url
-// mongoose
-//     .connect(process.env.MONGODB_URL, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     }).then(() => {
-//         console.log("Connected to MongoDB");
-//     }).catch((err) => {
-//         console.log(err);
-//     })
+mongoose
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("Connected to MongoDB");
+    }).catch((err) => {
+        console.log(err);
+    })
 
 // localhost
-mongoose.set('strictQuery', true);
-mongoose.connect(`${process.env.MONGO_URL}`, ()=>{
-    console.log("mongodb is connected")
-});
+// mongoose.set('strictQuery', true);
+// await mongoose.connect(`${process.env.MONGO_URL}`, ()=>{
+//     console.log("mongodb is connected")
+// });
